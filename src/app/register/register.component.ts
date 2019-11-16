7785fe3054e3348743f19c72b156950cbd29b53d
Divya Shah <56585827+readytomove@users.noreply.github.com>
2019-11-17 03:11:56 +0530

    project

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../auth.service";
+import {ToastrService} from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  
+  
+  constructor( private Auth: AuthService, private Toastr : ToastrService) {  }
 
-  ngOnInit() {
+  
+
+  ngOnInit() { }
+  
+  RegisterDataEntry(event) {
+    console.log("Inside signUpDataEntry");
+    event.preventDefault();
+    const target = event.target;
+    var errorList = [];
+
+    const fname = target.querySelector("#firstname").value;
+
+    const lname = target.querySelector("#lastname").value;
+
+    const email = target.querySelector("#email").value;
+
+    const number = target.querySelector("#mnumber").value;
+
+    const pass = target.querySelector("#password").value;
+    const cpass = target.querySelector("#cpassword").value;
+    
+    if (pass === cpass) {
+      if (errorList.length === 0) this.Auth.userregister(fname,lname, email,number, pass);
+    } else {
+      this.Toastr.error("Password and Confirm Password Must match", "Error", {
+        progressBar: true
+      });
+      console.log("error in component");
+    }
   }
-
 }
